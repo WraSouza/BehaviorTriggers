56537d1703ef08ad451bd688f2aa2c7cdf8ef996
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2021-07-01 15:24:27 -0300

    Triggers no botão, em texto inserido.

---
+++ BehaviorTriggers/BehaviorTriggers/lib/EmailValidoTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BehaviorTriggers.lib
+{
+    public class EmailValidoTrigger : TriggerAction<Entry>
+    {
+        //É necessário implementar essa método
+        protected override void Invoke(Entry sender)
+        {
+            if(sender.Text.IndexOf('@') >= 0)
+            {
+                sender.TextColor = Color.Green;
+            }
+            else
+            {
+                sender.TextColor = Color.Red;
+            }
+           
+        }
+    }
+}
